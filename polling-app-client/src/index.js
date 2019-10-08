027f183590166872b883c250a04ecbecb699bbe5
Marco Mera <marcomera@icloud.com>
2019-10-07 21:54:15 -0500

    Updated APIUtils code

--- polling-app-client/src/index.js
+++ polling-app-client/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
+
 ReactDOM.render(
     <Router>
         <App />
